9b8dd31408e574c2b31d8d5e8986e7e6b43a6e9c
msahrana <msahrana@gmail.com>
2023-10-20 12:48:41 +0600

    login page create of my site

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDE1YQAlteisMQPtkeGO4o30_PFVi_ccuw",
+  authDomain: "siyana-carvila.firebaseapp.com",
+  projectId: "siyana-carvila",
+  storageBucket: "siyana-carvila.appspot.com",
+  messagingSenderId: "370789477093",
+  appId: "1:370789477093:web:0b60adfd9d118d8818020c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,81 @@
 
 
 const Register = () => {
+
+    const handleSingUp = e =>{
+        e.preventDefault()
+        const form = e.target 
+        const name = form.name.value 
+        const photo = form.photo.value 
+        const email = form.email.value 
+        const password = form.password.value 
+        console.log(name, photo, email, password)
+    }
+
     return (
-        <div>
-            Register page 
+        <div className="hero min-h-screen bg-lime-50 -my-24">
+      <div className="hero-content flex-col ">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold text-orange-500"> SignUp!</h1>
         </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleSingUp} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text" name="name"
+                placeholder="Name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="text" name="photo"
+                placeholder="Photo URL"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email" name="email"
+                placeholder="Email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password" name="password"
+                placeholder="Password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">SignUp</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
     );
 };
 
